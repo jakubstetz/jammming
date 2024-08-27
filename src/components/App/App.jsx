3654b616453e4d80605065f45350608b03b8a3f7
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-08-27 18:28:42 -0400

    Render and style search bar.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,14 @@
+import SearchBar from "../SearchBar/SearchBar"
+
 function App() {
   return (
     <>
       <header className="header">
         <h1 className="centered-text">Ja<span>mmm</span>ing</h1>
       </header>
+      <main>
+        <SearchBar />
+      </main>
     </>
   )
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,9 @@
 function SearchBar() {
   return (
-    <>
-
-    </>
+    <div id="search-bar">
+      <label htmlFor="search">Search:</label>
+      <input id="search" placeholder="Search something here!" />
+    </div>
   )
 }
 
