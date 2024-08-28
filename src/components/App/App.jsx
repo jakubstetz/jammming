bec42f803031f0b27e38d679f7b8ca482ddd10eb
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-08-28 09:04:45 -0400

    Implement state and handler for tracking and updating playlist name.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,11 +5,17 @@ import Playlist from '../Playlist/Playlist';
 import {testSongs} from '../../mocks/spotifyMock';
 
 function App() {
+  ///// States /////
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState(testSongs);
+  const [playlistName, setPlaylistName] = useState("");
 
+  ///// Handlers /////
   const changeHandler = ({target: {value}}) => setSearch(value); // Track user input in SearchBar component.
 
+  const playlistNameHandler = ({target: {value}}) => setPlaylistName(value); // Tracking the name of the user's under-construction playlist.
+
+  ///// Returned Component /////
   return (
     <>
       <header className="header">
@@ -19,7 +25,7 @@ function App() {
         <SearchBar value={search} onChange={changeHandler} />
         <div id="search-results-and-playlist">
           <SearchResults search={search} tracks={searchResults} />
-          <Playlist tracks={searchResults} />
+          <Playlist tracks={searchResults} value={playlistName} playlistHandler={playlistNameHandler} />
         </div>
       </main>
     </>
--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,11 +1,11 @@
 import Tracklist from "../Tracklist/Tracklist";
 
-function Playlist({tracks}) {
+function Playlist({tracks, value, playlistHandler}) {
   return (
     <div className="tracks-container">
       <div className="tracks-container-header">
         <p className="centered-text" >Playlist Name</p>
-        <input type="text" className="centered-text"></input>
+        <input type="text" className="centered-text" value={value} onChange={playlistHandler}></input>
       </div>
       <Tracklist tracks={tracks} />
       <div id="save-to-spotify">
