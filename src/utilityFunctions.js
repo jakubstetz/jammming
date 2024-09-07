fece2e7ac2c0d985383f6701e6a83408210979f6
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-09-07 15:04:21 -0400

    Remove console.log() call in searchSpotify() function that was being used for testing purposes.

--- src/utilityFunctions.js
+++ src/utilityFunctions.js
@@ -84,7 +84,6 @@ export const spotifyFunctions = {
     if (!response.ok) throw new Error(`Response status: ${response.status}`);
 
     const json = await response.json();
-    console.log(json);
     return json;
   }
 }
