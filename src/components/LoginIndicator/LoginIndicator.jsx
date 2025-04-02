819e99119a8cbd51437df3ccecd43ca7d835a821
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-01 21:59:44 -0700

    Adjust Login Indicator component to use higher-resolution user profile image

--- src/components/LoginIndicator/LoginIndicator.jsx
+++ src/components/LoginIndicator/LoginIndicator.jsx
@@ -1,7 +1,7 @@
 function LoginIndicator({userInfo: {display_name, images}}) {
   return (
     <div id="login-indicator">
-      <img src={images ? images[1].url : ''} />
+      <img src={images ? images[0].url : ''} />
       <div>
         <p>Logged in on Spotify as:</p>
         <h2>{display_name ? display_name : ''}</h2>
